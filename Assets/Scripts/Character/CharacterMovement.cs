98f0ddf7cc71b577057a722b98871d15ffc89ebf
Mehran Akhavan <mehran.akhavan.1994@gmail.com>
2022-04-02 11:44:07 +0430

    Init Commit with basic movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+
+    [SerializeField]
+    private float velocity;
+
+    private Vector2 speed;
+        
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        speed = new Vector2(horizontalInput * velocity, verticalInput * velocity);
+
+        transform.Translate(speed * Time.deltaTime);
+    }
+}
